92447f5ad5f1791c543d194ed0861a52a7d07d4c
datcroft-assignment <datcroft-assignment@yandex.ru>
2016-08-20 11:29:57 +0300

    Circle added. Circle falls down and destroy on bottom of the screen.

---
+++ circles/Assets/scripts/CircleBhv.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleBhv : MonoBehaviour
+{
+    public FromToFloat Speed;
+    public FromToFloat Size;
+
+    private float _speed;
+    private float _size;
+
+    void Start ()
+    {
+        float rndFloat = Random.value;
+        _size = Size.Lerp(rndFloat); // получаем случайное значение размера
+        transform.localScale *= _size;
+        _speed = Speed.Lerp(1 - rndFloat); // значение скорости, соответствующее размеру
+        // случайное расположение кружка вдоль верхней границы экрана
+        float randPosAcrossScreen = 2 * (Random.value - 0.5f) * (Camera.main.aspect * Camera.main.orthographicSize - _size);
+        transform.position = new Vector3(randPosAcrossScreen, Camera.main.orthographicSize - _size, 0);
+    }
+
+	void Update ()
+    {
+        transform.position -= new Vector3(0, _speed * Time.deltaTime, 0); // движение вниз
+        if (transform.position.y < -Camera.main.orthographicSize + _size)
+            Destroy(gameObject); // проверка на достижение нижней границы экрана
+    }
+}
---
+++ circles/Assets/scripts/FromToFloat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+[Serializable]
+public class FromToFloat
+{
+    public float From;
+    public float To;
+
+    public float Lerp(float k)
+    {
+        return Mathf.Lerp(From, To, k);
+    }
+}
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(FromToFloat))]
+public class FromToFloatDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+        EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+        var fromRect = new Rect(80, position.y, 40, position.height);
+        var toRect = new Rect(140, position.y, 40, position.height);
+        EditorGUI.PropertyField(fromRect, property.FindPropertyRelative("From"), GUIContent.none);
+        EditorGUI.PropertyField(toRect, property.FindPropertyRelative("To"), GUIContent.none);
+        EditorGUI.EndProperty();
+    }
+}
+#endif
